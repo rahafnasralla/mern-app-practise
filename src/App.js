804c9a3d0708810cc39a4b5b1b0e1ac41dc0ff77
rahafnasralla <nasrallahrahaf7@gmail.com>
2023-04-29 12:23:45 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState, useEffect} from 'react'
+import Axios from 'axios'
 function App() {
+
+  const [kids, setKids] = useState([])
+  const [name,setName] = useState('')
+  const [age,setAge] = useState(0)
+
+  useEffect(()=> {
+    Axios.get('http://localhost:5000/getKids').then((response) => {
+      setKids(response.data)
+    })
+      
+  },[])
+
+  const createKid = () => {
+    Axios.post('http://localhost:5000/createkid',
+     {name, age}).then((response)=> {
+      alert('Kid Created')
+      setKids([...kids,{name,age}])
+    })
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div>
+    {kids.map((kid)=> {
+      return (
+         <div>
+          <h1>Name : {kid.name}</h1>
+          <h2>Age : {kid.age}</h2>
+         </div>
+      )
+    })}
+    </div>
+
+    <div>
+      <input type="text" placeholder='name' onChange={(event)=> {setName(event.target.value)}} />
+      <input type="number" placeholder='age' onChange={(event)=> {setAge(event.target.value)}} />
+      <button onClick={createKid}>create kid</button>
+    </div>
+
     </div>
   );
 }
